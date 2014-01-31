ffba739967b9e79f908ae7a211e523519a2c7d63
manish05 <manish@newsinshorts.com>
2014-02-01 04:59:58 +0530

    mattrix

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		for(var i=0;i<100;i++){
 			content+="<tr>";
 			for(var j=0;j<100;j++){
-				content+="<td data-wish='Cell("+(i+1)+","+(j+1)+")'></td>";
+				content+="<td data-wish='Cell "+(i+1)+","+(j+1)+"'></td>";
 			}
 			content+="</tr>";
 		}
