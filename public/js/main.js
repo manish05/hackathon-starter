9b48a0cc86f1599bc645f19c19fb5583dc375df4
manish05 <manish@newsinshorts.com>
2014-02-01 03:57:14 +0530

    sdfsdf

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
 
   // Place JavaScript code here...
 	if($("#dataTable").length){
-	
-	 alert($("#dataTable").html());
-	 
+		
+		var content="";
+		for(var i=0;i<100;i++){
+			content+="<tr>";
+			for(var j=0;j<100;j++){
+				content+="<td>"+i+","+j+"</td>";
+			}
+			content+="</tr>";
+		}
 	}
 });
