582a5e781de1ffbbaef298851bdbf15679e47d00
manish05 <manish@newsinshorts.com>
2014-02-01 12:00:20 +0530

    yy

--- public/js/main.js
+++ public/js/main.js
@@ -7,25 +7,37 @@ $(document).ready(function() {
 		for(var i=0;i<100;i++){
 			content+="<div class='trr'>";
 			for(var j=0;j<100;j++){
-				content+="<div class='tdd' >X</div>";
+				content+="<div class='tdd' data-wish='Cell "+(i+1)+","+(j+1)+"' ></div>";
 			}
 			content+="</div>";
 		}
 		$("#dataTable").html(content);
 		
 		$(window).mousemove(function(event){
-			var t=$('#dataTable').offset().top- $(window).scrollTop(),
-				l=$('#dataTable').offset().left- $(window).scrollLeft();
+			var t=-1,l=-1,r=-1,b=-1;
+			try
+			{
+			t=$('#dataTable').offset().top- $(window).scrollTop(),
+			l=$('#dataTable').offset().left- $(window).scrollLeft();
+			r=l+1000;
+			b=t+1000;
+			}
+			catch(e)
+			{
+			return;
+			}
 			var curX=event.pageX,curY=event.pageY;
-			console.log(curX+" "+curY+"\n");
+			if(t<=curY && l<=curX && b>=curY && r>=curX){
+				var i,j;
+				
+				$(".wishTip").html("Buy This");
+				$(".wishTip").css("top",curY+$(window).scrollTop());
+				$(".wishTip").css("left",curX+$(window).scrollLeft());
+				
+				$(".wishTip").show();
+			}
+			else{
+				$(".wishTip").hide();
+			}
 		});
 });
-
-function showTip(i,j,msg)
-{
-	if(!msg)msg="Cell "+(i+1)+","+(j+1)+";
-	var w=$(".wishTip");
-	w.html(msg);
-	w.show();
-	
-}
