554c48875929909fc4c4ac59ac2bc3ce42790197
manish05 <manish@newsinshorts.com>
2014-02-01 11:11:25 +0530

    hmm

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+window.onerror = function (mesg, url, num) {return true;}
 $(document).ready(function() {
 
   // Place JavaScript code here...
@@ -6,10 +7,25 @@ $(document).ready(function() {
 		for(var i=0;i<100;i++){
 			content+="<div class='trr'>";
 			for(var j=0;j<100;j++){
-				content+="<div class='tdd' data-wish='Cell "+(i+1)+","+(j+1)+"'></div>";
+				content+="<div class='tdd' onhover='showTip("+i+","+j+")'></div>";
 			}
 			content+="</div>";
 		}
 		$("#dataTable").html(content);
-	
+		
+		$(window).mousemove(function(event){
+			var t=$('#dataTable').offset().top- $(window).scrollTop(),
+				l=$('#dataTable').offset().left- $(window).scrollLeft();
+			var curX=event.pageX,curY=event.pageY;
+			
+		});
 });
+
+function showTip(i,j,msg)
+{
+	if(!msg)msg="Cell "+(i+1)+","+(j+1)+";
+	var w=$(".wishTip");
+	w.html(msg);
+	w.show();
+	
+}
