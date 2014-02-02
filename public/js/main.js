a0baee60fa9d3f276638404048c610f4cceb320f
manish05 <manish@newsinshorts.com>
2014-02-02 12:56:09 +0530

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -2,25 +2,39 @@ window.onerror = function (mesg, url, num) {return true;}
 $(document).ready(function() {
 
   // Place JavaScript code here...
-		
+		var mannatData=0;
 		var content="";
+		var rectSi,rectEi,rectSj,rectEj;
+		function rectClick(i,j)
+		{
+			if(mannatData!=0){
+			
+				if(mannatData[i][j].s!='x')return;
+				
+				$(".tdd"+i+"+","+"+j).toggleClass("selected");
+			}
+		}
 		for(var i=0;i<20;i++){
 			content+="<div class='trr'>";
 			for(var j=0;j<20;j++){
 				content+="<div class='tdd tdd"+i+"+","+"+j+"' data-wish='Cell "+(i+1)+","+(j+1)+"' ></div>";
+				
+				$(".tdd"+i+"+","+"+j").dblclick(rectClick(i,j));
 			}
 			content+="</div>";
 		}
 		$("#dataTable").html(content);
 		
 		$.getJSON("/mannatData",function(data)){
+			mannatData=data;
 			for(var i=0;i<200;i++)
 			 for(var j=0;j<200;j++){
 				if(data[i][j].s!='x'){
-					$(".tdd"+i+"+","+"+j).css("background","url('/img/"+data[i][j].s+"')");
+					$(".tdd"+i+"+","+"+j).css("background","url('/img/"+data[i][j].s+".jpg')");
 				}
 			 }
 		});
+		
 		
 		$(".tdd").qtip({
 			overwrite: false,
